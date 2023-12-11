fc22d6b8a9f55196e0fa40e4c285e6104ca07155
CryptoFist <jonekell1002@gmail.com>
2023-12-11 06:19:59 +0100

    redeployed contracts

--- scripts/params.js
+++ scripts/params.js
@@ -3,12 +3,12 @@ const { network, ethers } = require("hardhat");
 const DEPLOYMENT_PARAM = {
     goerli: {
         lzEndPoint: "0xbfD2135BFfbb0B5378b56643c2Df8a87552Bfa23",
-        minGasToTransfer: ethers.utils.parseEther("0.0001"),
+        minGasToTransfer: 150000,
         dstChainId: 10106,
     },
     fuji: {
         lzEndPoint: "0x93f54D755A063cE7bB9e6Ac47Eccc8e33411d706",
-        minGasToTransfer: ethers.utils.parseEther("0.0001"),
+        minGasToTransfer: 150000,
         dstChainId: 10121,
     },
 };
